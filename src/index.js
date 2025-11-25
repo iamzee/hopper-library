494a334b7b86a906f4c4a92c0c3ac7176e23660b
Zeeshan Adeen <zeeshan.adeen@newtonschool.co>
2025-11-25 13:24:52 +0530

    feat: add dotenv and POST genre route

--- src/index.js
+++ src/index.js
@@ -1,9 +1,25 @@
+require("dotenv").config();
 const express = require("express");
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
+
 
 const app = express();
 
+app.use(express.json());
+
 app.get("/health", (req, res) => {
   res.send("OK")
+})
+
+app.post("/api/v1/genres", async (req, res) => {
+  const body = req.body;
+  let genre = await prisma.genre.create({
+    data: {
+      name: body.name
+    }
+  })
+  res.status(201).json(genre);
 })
 
 app.listen(3003, () => {
