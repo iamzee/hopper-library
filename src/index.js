339e3da27a87dadde5650551b1fcea5da1c3a62d
Zeeshan Adeen <zeeshan.adeen@newtonschool.co>
2025-11-26 11:35:35 +0530

    feat: genre routes

--- src/index.js
+++ src/index.js
@@ -26,6 +26,43 @@ app.get("/api/v1/genres", async (req, res) => {
   return res.status(200).json(genres);
 })
 
+app.get("/api/v1/genres/:id", async (req, res) => {
+  const id = Number(req.params.id);
+  const genre = await prisma.genre.findUnique({
+    where: {
+      id
+    }
+  })
+
+  if (!genre) {
+    return res.status(404).json({ error: "Genre with the given id does not exist" })
+  }
+  res.status(200).json(genre)
+})
+
+app.patch("/api/v1/genres/:id", async (req, res) => {
+  const id = req.params.id;
+  await prisma.genre.update({
+    where: {
+      id: id
+    },
+    data: req.body
+  })
+
+  return res.status(200).send("OK")
+})
+
+app.delete("/api/v1/genres/:id", async (req, res) => {
+  const id = Number(req.params.id);
+  await prisma.genre.delete({
+    where: {
+      id: id
+    }
+  })
+
+  return res.status(204).send();
+})
+
 
 app.listen(3003, () => {
   console.log("Server started on port 3003")
