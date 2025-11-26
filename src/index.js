7d6f08a22baaf5dc43ebb6c25c8a0c1b167beca1
Zeeshan Adeen <zeeshan.adeen@newtonschool.co>
2025-11-26 11:23:34 +0530

    feat: GET /genres

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ const express = require("express");
 const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
-
 const app = express();
 
 app.use(express.json());
@@ -21,6 +20,12 @@ app.post("/api/v1/genres", async (req, res) => {
   })
   res.status(201).json(genre);
 })
+
+app.get("/api/v1/genres", async (req, res) => {
+  const genres = await prisma.genre.findMany();
+  return res.status(200).json(genres);
+})
+
 
 app.listen(3003, () => {
   console.log("Server started on port 3003")
